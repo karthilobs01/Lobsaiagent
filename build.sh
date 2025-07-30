6032ee86bb545aa6751a1b7def563ff98ea58ff7
karthilobs01 <karthikeyan@laabamone.com>
2025-07-30 16:21:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
-# Exit on error
 set -e
 
-# Install PortAudio and FFMPEG
-apt-get update && \
-apt-get install -y portaudio19-dev ffmpeg
+apt-get update
+apt-get install -y libportaudio2 portaudio19-dev ffmpeg
